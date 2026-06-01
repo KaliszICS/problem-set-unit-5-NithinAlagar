eafa10745933ea0e097d7ad5de7a3623c07c6e1e
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2026-05-31 22:11:47 -0400

    add feedback

--- src/main/java/TextAnalyzer.java
+++ src/main/java/TextAnalyzer.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class ProblemSet {
+public class TextAnalyzer {
 
 	public static void main(String args[]) {
         System.out.println("Welcome to the Text Scrutinizor!");
